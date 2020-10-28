bf6c6ba184f1c2df4eb8de66c16059f2e02fed95
pollra <pollra32@naver.com>
2020-10-29 01:37:55 +0900

    Initial commit

---
+++ src/main/java/com/pollra/pudding/PuddingBackApplication.java
@@ -0,0 +1,13 @@
+package com.pollra.pudding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuddingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuddingBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pollra/pudding/PuddingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollra.pudding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuddingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
