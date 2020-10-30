f9de78e9d90c6eb86ded70effe65f51ade570cc4
pollra <pollra32@naver.com>
2020-10-31 00:07:07 +0900

    푸딩-엔진 : ExceptionCode 추가

--- src/main/java/com/pollra/pudding/common/engine/exception/ExceptionCode.java
+++ src/main/java/com/pollra/pudding/common/engine/exception/ExceptionCode.java
@@ -1,5 +1,10 @@
 package com.pollra.pudding.common.engine.exception;
 
 public enum ExceptionCode {
-    E00000000 // 아직 에러 명시 ㄴㄴ
+
+    /**
+     * 공통
+     */
+     E00010001  // 유효성 검사에 실패하였습니다
+    ,E00010002  // 해당 정보를 찾을 수 없습니다
 }
