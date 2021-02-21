ef93c62a61b02328f32580c8cdd3a9cf6fac07ff
pollra <pollra32@naver.com>
2021-02-21 19:38:06 +0900

    푸딩 : Property 암호화 적용

--- src/main/java/com/pollra/pudding/PuddingBackApplication.java
+++ src/main/java/com/pollra/pudding/PuddingBackApplication.java
@@ -1,11 +1,13 @@
 package com.pollra.pudding;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses=PuddingBackApplication.class)
+@EncryptablePropertySource({"classpath:application.yml"})
 public class PuddingBackApplication {
 
     public static void main(String[] args) {
