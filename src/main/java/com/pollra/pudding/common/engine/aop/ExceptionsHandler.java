a78bb4c48e7c27f14884d398e98ce914e4f5ea5e
pollra <pollra32@naver.com>
2021-02-22 10:14:33 +0900

    푸딩 : JWT 관련 ExceptionsHandler 구현

--- src/main/java/com/pollra/pudding/common/engine/aop/ExceptionsHandler.java
+++ src/main/java/com/pollra/pudding/common/engine/aop/ExceptionsHandler.java
@@ -1,8 +1,11 @@
 package com.pollra.pudding.common.engine.aop;
 
 import com.pollra.pudding.common.engine.constant.Constant;
+import com.pollra.pudding.common.engine.exception.ExceptionCode;
 import com.pollra.pudding.common.engine.exception.ExceptionForm;
 import com.pollra.pudding.common.engine.helper.exception.ExceptionHelper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 
 @Slf4j
@@ -23,6 +27,24 @@ public class ExceptionsHandler {
         }
         log.error(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NETWORK_AUTHENTICATION_REQUIRED);
+    }
+
+    @ExceptionHandler(UnsupportedEncodingException.class)
+    public ResponseEntity<?> handleUnsupportedEncodingException(UnsupportedEncodingException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(ExceptionHelper.getException(ExceptionCode.E00020003), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<?> handleExpiredJwtException(ExpiredJwtException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(ExceptionHelper.getException(ExceptionCode.E00020002), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ResponseEntity<?> handleSignatureException(SignatureException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(ExceptionHelper.getException(ExceptionCode.E00020001), HttpStatus.BAD_REQUEST);
     }
 
 }
