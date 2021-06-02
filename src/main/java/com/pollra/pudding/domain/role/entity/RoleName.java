1b83203d9facd1b0a96e6e6ab1b2f415ff345f1f
pollra <live-niceday-pollar-at-773830787333>
2021-06-02 09:31:55 +0900

    RoleName : 권한 이름의 불변식이 제대로 적용되지 않던 문제 해결

--- src/main/java/com/pollra/pudding/domain/role/entity/RoleName.java
+++ src/main/java/com/pollra/pudding/domain/role/entity/RoleName.java
@@ -1,16 +1,15 @@
 package com.pollra.pudding.domain.role.entity;
 
-import com.pollra.pudding.common.engine.exception.ExceptionCode;
 import com.pollra.pudding.domain.acl.enumerated.AuthorityCode;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.http.HttpStatus;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 
-import static com.pollra.pudding.common.engine.util.StringScopeUtil.betweenIs;
+import static com.pollra.pudding.common.engine.util.StringScopeUtil.notBetweenIs;
 
 /**
  * @since       2021.04.14
@@ -26,7 +25,7 @@ public class RoleName {
 	private String name;
 	
 	private RoleName(final String name, AuthorityCode authority) {
-		if(StringUtils.isNotBlank(name) && betweenIs(name, 2, 20) ) {
+		if(StringUtils.isNotBlank(name) && notBetweenIs(name, 2, 20) ) {
 			throw new IllegalArgumentException("권한 이름을 다시 확인해주세요.");
 		}
 		this.name = name.toUpperCase()+"_"+authority;
