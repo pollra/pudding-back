1864f92fe3e4882266cd775ee782c1b8e230566e
pollra <live-niceday-pollar-at-773830787333>
2021-06-11 19:39:03 +0900

    Role : 기본 RoleName 의 name 속성을 DEFAULT 로 작성

--- src/main/java/com/pollra/pudding/domain/role/entity/RoleFactory.java
+++ src/main/java/com/pollra/pudding/domain/role/entity/RoleFactory.java
@@ -2,7 +2,7 @@ package com.pollra.pudding.domain.role.entity;
 
 public class RoleFactory {
     public static Role guestRole() {
-        return new Role(new RoleName("", AuthorityCode.GUEST));
+        return new Role(new RoleName("DEFAULT", AuthorityCode.GUEST));
     }
 
     public static Role newRole(final String name, final AuthorityCode authorityCode) {
