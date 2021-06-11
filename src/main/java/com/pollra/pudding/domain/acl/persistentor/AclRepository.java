fb185fdc113a8740ff694e18ce6c7d401e89568e
pollra <live-niceday-pollar-at-773830787333>
2021-06-11 19:46:16 +0900

    Acl : 패키지 변경 repository -> persistentor

--- src/main/java/com/pollra/pudding/domain/acl/persistentor/AclRepository.java
+++ src/main/java/com/pollra/pudding/domain/acl/persistentor/AclRepository.java
@@ -1,12 +1,12 @@
-package com.pollra.pudding.domain.acl.repository;
+package com.pollra.pudding.domain.acl.persistentor;
 
 import com.pollra.pudding.domain.acl.entity.Acl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * @author pollra
+ * @since       2021.06.02
+ * @author      pollra
  * @description acl repository
- * @since 2021.06.02
  **********************************************************************************************************************/
 public interface AclRepository extends JpaRepository<Acl, Long> {
 }
